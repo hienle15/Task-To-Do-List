597e35fc248b3c7f1199536f9666c00e8d327fd3
hienle15 <hienle88995@gmail.com>
2025-05-13 15:54:59 +0700

    Update server configuration in vite.config.js to specify allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-     allowedHosts: 'all',
+    server: {
+    allowedHosts: ['task-to-do-list-cfnq.onrender.com'],
+    host: true, // hoặc '0.0.0.0' nếu cần
   },
 })
